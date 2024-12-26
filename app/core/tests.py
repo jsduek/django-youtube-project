4c7da0ecb7690b4474b5c0605cf031f086c02f0f
jsduek <jsduek28@gmail.com>
2024-12-26 15:40:14 +0900

    change

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,36 +1,29 @@
-"""
-Test custom Django management commands.
-"""
-from unittest.mock import patch
+# docker-compose run --rm app sh -c 'python manage.py test core'
 
-from psycopg2 import OperationalError as Psycopg2OPsycopgpError
-
+from django.test import SimpleTestCase
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCase
+from psycopg2 import OperationalError as Psycopg2OperationalError
+from unittest.mock import patch # 중간에 가로채는 기능
 
-
+#db연결 시도를 가로챔
 @patch('django.db.utils.ConnectionHandler.__getitem__')
-class CommandTests(SimpleTestCase):
-    """Test commands."""
+class CommendsTests(SimpleTestCase):
+	# DB가 준비되었을 때 wait_for_db가 잘 동작하는 지 체크하는 함수
+	def test_wait_for_db_ready(self, patched_getitem):
+		patched_getitem.return_value = True
 
-    # DB가 준비되었을 때 wait_for_db가 잘 동작하는 지 체크하는 함수
-    def test_wait_for_db_ready(self, patched_getitem):
-        """Test waiting for database if database ready."""
-        patched_getitem.return_value = True
+		call_command('wait_for_db')
 
-        call_command('wait_for_db')
+		self.assertEqual(patched_getitem.call_count, 1)
+		
+	# DB연결에 오류가 발생했다고 가정을 하고 테스트
+	@patch('time.sleep')
+	def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
+		# Psycopg2에러 1번 OperationalError 5번 후 True
+		patched_getitem.side_effect = [Psycopg2OperationalError] + \
+			[OperationalError] * 5 + [True]
 
-        self.assertEqual(patched_getitem.call_count, 1)
+		call_command('wait_for_db')
 
-    @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
-        """Test waiting for database when getting OperationalError."""
-        patched_getitem.side_effect = [Psycopg2OPsycopgpError] + \
-            [OperationalError] * 5 + [True]
-
-        call_command('wait_for_db')
-
-        self.assertEqual(patched_getitem.call_count, 7)
-
-# docker-compose run --rm app sh -c 'python manage.py test core'
+		self.assertEqual(patched_getitem.call_count, 7)
